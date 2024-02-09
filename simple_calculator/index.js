59be5e0653d28955bd0ab41b555e6025c9f3de05
pratyashasharma <sharmapratyasha1@gmail.com>
2024-02-08 20:29:09 -0500

    day 1 and day 2 first commit

---
+++ simple_calculator/index.js
@@ -0,0 +1,10 @@
+
+//todo 1.function to read and store the values
+//todo 2.function to display the progress the values
+//todo 3.function to read and store the values
+//todo 4.function to calculate the total operation
+//todo 5. function to handle mouse listeners
+//todo 5. function to handle key listeners
+//todo 6. function to handle invalid input
+
+
